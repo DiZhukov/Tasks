f81ee701ac00a78b38b6b9cc393b105ef4923524
DiZhukov <dizhukov96@mail.ru>
2021-12-20 20:35:25 +0300

    first commit

---
+++ Tasks/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  Tasks
+//
+//  Created by Dima Zhukov on 12.12.21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
